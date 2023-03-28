5e62bfe844ee649eec6c684e497795db69ace715
Fabricio Kassardjian <fkay13@gmail.com>
2023-03-28 12:51:25 -0300

    organizing files

---
+++ automata.py
@@ -0,0 +1,95 @@
+import pygame
+
+cell_color = [
+        (255, 255, 255),  # 0 = white
+        (0, 255, 0),  # 1 - green
+        (255, 0, 0),  # 2 - curr pos - red
+        (255, 255, 0),  # 3 - start - yellow
+        (255, 255, 0)  # 4 - end - yellow
+    ]
+
+
+# Load the map
+def load_init_map():
+    with open(r'./inputs/input_stone_automata.txt', encoding='utf-8') as file:
+        rows = file.readlines()
+    init_map = []
+    for i, r in enumerate(rows):
+        init_map.append([int(v) for v in r.split()])
+    return init_map
+
+
+def draw_map(screen: pygame.Surface, map: list,
+             cell_size: tuple[int, int]) -> None:
+    """
+    draw the map in a pygame surface
+
+    Args:
+        screen (pygame.Surface): surface to draw
+        map (list[][] of int): 2d matriz with map to draw
+        cell_size (tuple[int, int]): size of each cell (width, height)
+    """
+    for i_y, row in enumerate(map):
+        for i_x, cell in enumerate(row):
+            pygame.draw.rect(screen, (0, 0, 0),
+                             (i_x * cell_size[0], i_y * cell_size[1],
+                              cell_size[0], cell_size[1]))
+            pygame.draw.rect(screen, cell_color[cell],
+                             (i_x * cell_size[0] + 1, i_y * cell_size[1] + 1,
+                              cell_size[0] - 2, cell_size[1] - 2))
+
+
+def next_step_map(old_map: list) -> list:
+    """
+    Calculates next map based on automata rules
+    - White cells turn green if they have a number of adjacent green cells
+      greater than 1 and less than 5. Otherwise, they remain white.
+    - Green cells remain green if they have a number of green adjacent cells
+      greater than 3 and less than 6. Otherwise they become white.
+
+    Args:
+        old_map (list[][] of int): map on actual situation
+    Returns:
+        list[][] of int: new map after iteration
+    """
+    rows = len(old_map)
+    columns = len(old_map[0])
+    new_map = []
+    # assure start and end always white
+    old_map[0][0] = 0
+    old_map[rows - 1][columns - 1] = 0
+
+    for iy in range(0, rows):
+        new_row = []
+        for ix in range(0, columns):
+            # check adjacents
+            adjs = 0
+            if (iy != 0):
+                if (ix != 0):
+                    adjs += old_map[iy - 1][ix - 1]
+                adjs += old_map[iy - 1][ix]
+                if (ix != (columns - 1)):
+                    adjs += old_map[iy - 1][ix + 1]
+            if (iy != (rows - 1)):
+                if (ix != 0):
+                    adjs += old_map[iy + 1][ix - 1]
+                adjs += old_map[iy + 1][ix]
+                if (ix != (columns - 1)):
+                    adjs += old_map[iy + 1][ix + 1]
+            if (ix != 0):
+                adjs += old_map[iy][ix - 1]
+            if (ix != columns - 1):
+                adjs += old_map[iy][ix + 1]
+            new_cell = 0
+            if old_map[iy][ix] == 0:
+                if (adjs > 1) & (adjs < 5):
+                    new_cell = 1
+            else:
+                if (adjs > 3) & (adjs < 6):
+                    new_cell = 1
+            new_row.append(new_cell)
+        new_map.append(new_row)
+    # assure start and end colors
+    new_map[0][0] = 3
+    new_map[rows - 1][columns - 1] = 4
+    return new_map
--- stage2.py
+++ stage2.py
@@ -5,105 +5,85 @@ Uses pygame to visualize results
 
 # %% Import libraries
 import pygame
+from automata import load_init_map, next_step_map, draw_map
+from astar import solve_Astar
 
-# %% Load the map
-with open(r'./inputs/input_stone_automata.txt', encoding='utf-8') as file:
-    rows = file.readlines()
-init_map = []
-for i, r in enumerate(rows):
-    init_map.append([int(v) for v in r.split()])
+init_map = load_init_map()
 
-cell_color = [
-        (255, 255, 255),  # 0 = white
-        (0, 255, 0),  # 1 - green
-        (255, 0, 0),  # 2 - curr pos - red
-        (0, 255, 255),  # 3 - start - yellow
-        (0, 255, 255)  # 4 - end - yellow
-    ]
+hero = {
+    'x': 0,
+    'y': 0
+}
+
+hero_moves = []
 
 # %% Prepare pygame
 # Pygame Configuration
 pygame.init()
-fps = 300
+fps = 10
 fpsClock = pygame.time.Clock()
 width, height = 850, 650
-cell_width = width / 85
-cell_heigth = height / 65
+cell_size = (width / len(init_map[0]),
+             height / len(init_map))
 screen = pygame.display.set_mode((width, height), pygame.RESIZABLE)
 
 font = pygame.font.SysFont('Arial', 20)
 
-circle = {'radius': 30,
-          'pos_x': width / 2,
-          'pos_y': height / 2,
-          'speed_x': 1,
-          'speed_y': 2}
-
-
-def draw_map(screen: pygame.Surface, map: list) -> None:
-    for i_y, row in enumerate(map):
-        for i_x, cell in enumerate(row):
-            pygame.draw.rect(screen, (0, 0, 0),
-                             (i_x * cell_width, i_y * cell_heigth,
-                              cell_width, cell_heigth))
-            pygame.draw.rect(screen, cell_color[cell],
-                             (i_x * cell_width + 1, i_y * cell_heigth + 1,
-                              cell_width - 2, cell_heigth - 2))
-
-
-def next_step_map(old_map: list, rows: int, columns: int) -> list:
-    """
-    Calculates next map based on automata rules
-    - White cells turn green if they have a number of adjacent green cells
-      greater than 1 and less than 5. Otherwise, they remain white.
-    - Green cells remain green if they have a number of green adjacent cells
-      greater than 3 and less than 6. Otherwise they become white.
-
-    Args:
-        old_map (list[][] of int): map on actual situation
-        rows (int): how many rows are in the matrix
-        columns (int): how many columns in the matrix
-    Returns:
-        list[][] of int: new map after iteration
-    """
-    new_map = []
-    # check corners
-    c00 = old_map[1][0] + old_map[1][1] + old_map[0][1]
-    c01 = (old_map[1][columns - 1] +
-           old_map[1][columns - 2] +
-           old_map[0][columns - 2])
-    c10 = (old_map[rows - 2][0] +
-           old_map[rows - 2][1] +
-           old_map[rows - 1][1])
-    c11 = (old_map[rows - 2][columns - 1] +
-           old_map[rows - 2][columns - 2] +
-           old_map[rows - 1][columns - 2])
-
-    for iy in range(1, len(old_map) - 1):
-        new_row = (old_map[rows - 2][columns - 1] +
-                   old_map[rows - 2][columns - 2] +
-                   old_map[rows - 1][columns - 2])
-        for ix in range(1, len(old_map[0]) - 1):
-            # check adjacents
-            pass
-
-
 # %% Game loop
 running = True
+step = False
+continuous = False
+actual_map = init_map.copy()
+actual_map[hero["y"]][hero['x']] = 2
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                step = True
+            if event.key == pygame.K_a:
+                continuous = True
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                continuous = False
 
     # Fill the background with white
     screen.fill((255, 255, 255))
     # draw the map
-    draw_map(screen, init_map)
+    draw_map(screen, actual_map, cell_size)
 
     # Flip the display (render)
     pygame.display.flip()
 
+    # generate next automata
+    if step | continuous:
+        # clear hero position
+        actual_map[hero["y"]][hero['x']] = 0
+        actual_map = next_step_map(actual_map.copy())
+        if ((hero['x'] < len(actual_map[0])) &
+           (actual_map[hero["y"]][hero['x'] + 1] != 1)):
+            hero['x'] += 1
+            hero_moves.append('R')
+        elif ((hero['y'] < len(actual_map)) &
+              (actual_map[hero["y"] + 1][hero['x']] != 1)):
+            hero['y'] += 1
+            hero_moves.append('D')
+        elif ((hero['x'] > 0) &
+              (actual_map[hero["y"]][hero['x'] - 1] != 1)):
+            hero['x'] -= 1
+            hero_moves.append('L')
+        elif ((hero['y'] > 0) &
+              (actual_map[hero["y"] - 1][hero['x']] != 1)):
+            hero['y'] -= 1
+            hero_moves.append('U')
+        else:
+            hero_moves.append('X')
+        actual_map[hero["y"]][hero['x']] = 2
+        step = False
+
     # generate framerate
     fpsClock.tick(fps)
-
+print((' ').join(hero_moves))
 pygame.quit()
